5b4bb7b1d56646515277cc7d68b67db1ec14a703
chdrchz <saavannaa.rose@gmail.com>
2024-03-14 10:08:50 -0500

    adding total completed tasks, total number of tasks and formatted a print statement

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,9 +5,16 @@ import requests
 
 def employee_todo_list(employee_id):
     """This function displays todo list progress"""
-    url = "https://jsonplaceholder.typicode.com/"
-    employee_url = f"{url}/users/{employee_id}"
-    todo_url = f"{url}/todos"
+
+    site_url = "https://jsonplaceholder.typicode.com/"
+    employee_url = f"{site_url}/users/{employee_id}"
+    todo_url = f"{site_url}/todos"
 
     employee_name = requests.get(employee_url).json()
     todo_list = requests.get(todo_url, params={"userId": employee_id}).json()
+
+    completed_todos = [todo["title"] for todo in todo_list if todo["completed"]]
+    total_todos = len(todo_list)
+    total_done = len(completed_todos)
+
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, total_done, total_todos))
