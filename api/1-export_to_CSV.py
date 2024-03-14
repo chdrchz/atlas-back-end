ccfed7ecd0045e348a9f08d3ec525b40a12e6d96
chdrchz <saavannaa.rose@gmail.com>
2024-03-14 12:34:03 -0500

    rearranging employee data when fetching csv data

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -3,28 +3,32 @@
 import csv
 import os
 import requests
+import sys
 
 
 def employee_todo_list(employee_id):
     """This function exports to a csv"""
 
+    #API URLs
     site_url = "https://jsonplaceholder.typicode.com/"
     employee_url = f"{site_url}/users/{employee_id}"
     todo_url = f"{site_url}/todos"
 
+    # get employee data
     employee_data = requests.get(employee_url).json()
     employee_name = employee_data['name']
     todo_list = requests.get(todo_url, params={"userId": employee_id}).json()
 
-    completed_todos = [t["title"] for t in todo_list if t["completed"]]
-    total_todos = len(todo_list)
-    total_done = len(completed_todos)
+    # employees tasks
+    todo_list = requests.get(todo_url, params={"userId": employee_id}).json()
 
+    # prepare CSV data
     csv_data = [["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]]
     for todo in todo_list:
-        csv_row = [employee_id, employee_name, completed_todos, todo["title"]]
-        csv_data.append(csv_row)
-    # export to csv
+        task_completed_status = "Completed" if todo["completed"] else "Not Completed"
+        csv_data.append([employee_id, employee_name, task_completed_status, todo["title"]])
+    
+    # export to CSV
     csv_file_path = f"{employee_id}.csv"
     if os.path.exists(csv_file_path):
         print(f"File '{csv_file_path}' already exists. Not overwriting.")
@@ -32,10 +36,13 @@ def employee_todo_list(employee_id):
         with open(csv_file_path, 'w', newline='') as csv_file:
             writer = csv.writer(csv_file)
             writer.writerows(csv_data)
+        print(f"CSV file '{csv_file_path}' has been created successfully.")
 
 
 if __name__ == "__main__":
-    import sys
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
 
     employee_id = int(sys.argv[1])
     employee_todo_list(employee_id)
