4d62d2c9326463dbbfc3749602a58cc9009c00ce
chdrchz <saavannaa.rose@gmail.com>
2024-03-14 10:37:15 -0500

    going back to my code, and fixing the if name == main part

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,27 +3,28 @@
 import requests
 
 
-def display_employee_progress(employee_id):
-    """ script must display to stdout the employee todo list progress """
-    site_url = "https://jsonplaceholder.typicode.com"
+def employee_todo_list(employee_id):
+    """This function displays todo list progress"""
+
+    site_url = "https://jsonplaceholder.typicode.com/"
     employee_url = f"{site_url}/users/{employee_id}"
     todo_url = f"{site_url}/todos"
 
     employee_data = requests.get(employee_url).json()
-    todo_data = requests.get(todo_url,
-                             params={"userId": employee_id}).json()
+    employee_name = employee_data['name']
+    todo_list = requests.get(todo_url, params={"userId": employee_id}).json()
 
-    empl_name = employee_data.get("name")
-    completed_tasks = [t["title"] for t in todo_data if t["completed"]]
-    tasks_done = len(completed_tasks)
-    tasks_total = len(todo_data)
+    completed_todos = [t["title"] for t in todo_list if t["completed"]]
+    total_todos = len(todo_list)
+    total_done = len(completed_todos)
 
     print("Employee {} is done with tasks({}/{}):"
-          .format(empl_name, tasks_done, tasks_total))
-    for task in completed_tasks:
-        print(f"\t {task}")
-    
+          .format(employee_name, total_done, total_todos))
+
+    for todo in completed_todos:
+        print(f"\t {todo}")
+
 if __name__ == "__main__":
     import sys
 
-    display_employee_progress(int(sys.argv[1]))
+    employee_todo_list(int(sys.argv[1]))
