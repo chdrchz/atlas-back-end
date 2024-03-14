bf205797f6e1ba9aa1543d2421dab60aa4e29326
chdrchz <saavannaa.rose@gmail.com>
2024-03-14 14:21:31 -0500

    quick change, rewrote the json serialization

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -11,24 +11,22 @@ def employee_todo_list(employee_id):
     site_url = "https://jsonplaceholder.typicode.com"
     employee_url = f"{site_url}/users/{employee_id}"
     todo_url = f"{site_url}/todos"
-
     employee_data = requests.get(employee_url).json()
-    employee_name = employee_data['name']
+    username = employee_data.get('username')
+
     todo_list = requests.get(todo_url, params={"userId": employee_id}).json()
 
-    tasks = []
-    for todo in todo_list:
-        task = {
-            "task_id": todo["id"],
-            "title": todo["title"],
-            "completed": todo["completed"]
-        }
-        tasks.append(task)
+    data = [{
+        "task": todo['title'],
+        "completed": todo['completed'],
+        "username": username
+    } for todo in todo_list]
 
-    file_path = f"{employee_id}.json"
-    with open(file_path, 'w') as file:
-        json.dump(tasks, file, indent=4)
-        print(f"Data for employee_id {employee_id} witten to {file_path}.")
+    format = {str(employee_id): data}
+
+    file = f"{employee_id}.json"
+    with open(file, 'w') as f:
+        json.dump(format, f)
 
 
 if __name__ == "__main__":
