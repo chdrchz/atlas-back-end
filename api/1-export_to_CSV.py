a8629edae0b55ff63bc8215b5d3b6361f49df0c1
chdrchz <saavannaa.rose@gmail.com>
2024-03-14 13:13:57 -0500

    simplyfying code

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,58 +1,29 @@
 #!/usr/bin/python3
-"""This module defines a script that connects to an API"""
+""" This module defines a function that exports to a csv """
 import csv
-import os
 import requests
 import sys
 
 
 def employee_todo_list(employee_id):
-    """This function exports to a csv"""
+    """This function exports todo list tasks to a csv"""
 
-    # API URLs
-    site_url = "https://jsonplaceholder.typicode.com/"
+    site_url = "https://jsonplaceholder.typicode.com"
     employee_url = f"{site_url}/users/{employee_id}"
     todo_url = f"{site_url}/todos"
 
-    # get employee data
     employee_data = requests.get(employee_url).json()
-    employee_name = employee_data['name']
-    todo_list = requests.get(todo_url, params={"userId": employee_id}).json()
+    username = employee_data.get('username')
 
-    # employees tasks
-    todo_list = requests.get(todo_url, params={"userId": employee_id}).json()
-
-    # prepare CSV data
-    csv_data = [["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]]
-    for todo in todo_list:
-        if todo["completed"]:
-            task_completed_status = "Completed"
-        else:
-            task_completed_status = "Not Completed"
-        csv_row = [
-            employee_id,
-            employee_name,
-            task_completed_status,
-            todo["title"]
-        ]
-        csv_data.append(csv_row)
-
-    # export to CSV
-    csv_file_path = f"{employee_id}.csv"
-    if os.path.exists(csv_file_path):
-        print(f"File '{csv_file_path}' already exists. Not overwriting.")
-    else:
-        with open(csv_file_path, 'w', newline='') as csv_file:
-            writer = csv.writer(csv_file)
-            writer.writerows(csv_data)
-        num_tasks = len(csv_data) - 1
-        print(f"CSV file '{csv_file_path}' created with {num_tasks} tasks.")
+    todos = requests.get(todo_url, params={'userId': employee_id}).json()
+    csv_file_name = f"{employee_id}.csv"
+    with open(csv_file_name, mode='w', newline='') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for todo in todos:
+            writer.writerow([employee_id, username,
+                             todo['completed'], todo['title']])
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
 
-    employee_id = int(sys.argv[1])
-    employee_todo_list(employee_id)
+    employee_todo_list(int(sys.argv[1]))
