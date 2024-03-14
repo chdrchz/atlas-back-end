c4420b7f0b9e8f3a0aaf41f54d49733a6fb259d6
chdrchz <saavannaa.rose@gmail.com>
2024-03-14 11:32:47 -0500

    exporting data to a csv

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""This module defines a script that connects to an API"""
+import csv
+import requests
+
+
+def employee_todo_list(employee_id):
+    """
+    This function displays todo list progress
+    and exports to a csv
+    """
+
+    site_url = "https://jsonplaceholder.typicode.com/"
+    employee_url = f"{site_url}/users/{employee_id}"
+    todo_url = f"{site_url}/todos"
+
+    employee_data = requests.get(employee_url).json()
+    employee_name = employee_data['name']
+    todo_list = requests.get(todo_url, params={"userId": employee_id}).json()
+
+    completed_todos = [t["title"] for t in todo_list if t["completed"]]
+    total_todos = len(todo_list)
+    total_done = len(completed_todos)
+    
+    csv_data = [["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]]
+    for todo in todo_list:
+        csv_data.append([employee_id, employee_name, completed_todos, todo["title"]])
+    # export to csv
+    csv_file_path = f"{employee_id}.csv"
+    with open(csv_file_path, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerows(csv_data)
+
+if __name__ == "__main__":
+    import sys
+
+    employee_id = int(sys.argv[1])
+    employee_todo_list(employee_id)
